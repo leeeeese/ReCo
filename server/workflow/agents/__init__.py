25079ad4fd7a590cbcf8df3e96d23fee6cf36583
leeeeese <dudtjdwh@gmail.com>
2025-11-06 09:12:25 +0900

    복잡한 subagent 구조 통합 > 총 3개의 subagent

--- server/workflow/agents/__init__.py
+++ server/workflow/agents/__init__.py
@@ -1,23 +1,16 @@
 """
 워크플로우 Agents 모듈
+3개 서브에이전트만 유지
 """
 
-from .persona_classifier import persona_classifier_node
-from .query_generator import query_generator_node
 from .price_agent import price_agent_node
 from .safety_agent import safety_agent_node
 from .persona_matching_agent import persona_matching_agent_node
-from .final_matcher import final_matcher_node
-from .ranker import ranker_node
-from .sql_generator import SQLGenerator
+from .recommendation_orchestrator import recommendation_orchestrator_node
 
 __all__ = [
-    "persona_classifier_node",
-    "query_generator_node",
     "price_agent_node",
     "safety_agent_node",
     "persona_matching_agent_node",
-    "final_matcher_node",
-    "ranker_node",
-    "SQLGenerator",
+    "recommendation_orchestrator_node",
 ]
